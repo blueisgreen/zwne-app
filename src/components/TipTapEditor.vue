5046478ccc8e25757442c503f3e164f02baa283f
Dave Mount <davemount@gmail.com>
2023-03-01 16:55:46 -0800

    Park for today

--- src/components/TipTapEditor.vue
+++ src/components/TipTapEditor.vue
@@ -9,7 +9,6 @@
         icon="format_bold"
         dense
         size="sm"
-        class="gap"
       />
       <q-btn
         @click="editor.chain().focus().toggleItalic().run()"
@@ -19,7 +18,6 @@
         icon="format_italic"
         dense
         size="sm"
-        class="gap"
       />
       <q-btn
         @click="editor.chain().focus().toggleStrike().run()"
@@ -29,12 +27,25 @@
         icon="strikethrough_s"
         dense
         size="sm"
-        class="gap"
+      />
+      <q-btn
+        @click="editor.chain().focus().toggleBulletList().run()"
+        :color="getButtonColor('bulletList')"
+        :text-color="getTextColor('bulletList')"
+        icon="format_list_bulleted"
+        dense
+        size="sm"
+      />
+      <q-btn
+        @click="editor.chain().focus().toggleOrderedList().run()"
+        :color="getButtonColor('orderedList')"
+        :text-color="getTextColor('orderedList')"
+        icon="format_list_numbered"
+        dense
+        size="sm"
       />
     </q-toolbar>
-    <div class="frame">
-      <editor-content :editor="editor" />
-    </div>
+    <editor-content :editor="editor" />
   </div>
 </template>
 
@@ -81,11 +92,7 @@ export default {
 
   watch: {
     modelValue(value) {
-      // HTML
       const isSame = this.editor.getHTML() === value
-
-      // JSON
-      // const isSame = JSON.stringify(this.editor.getJSON()) === JSON.stringify(value)
 
       if (isSame) {
         return
@@ -121,5 +128,61 @@ export default {
 .frame {
   border: 5px groove burlywood;
   padding: 1em;
+}
+.ProseMirror {
+  > * + * {
+    margin-top: 0.75em;
+    padding: 0.5em;
+  }
+
+  ul,
+  ol {
+    padding: 0 1rem;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    line-height: 1.1;
+  }
+
+  code {
+    background-color: rgba(#616161, 0.1);
+    color: #616161;
+  }
+
+  pre {
+    background: #0d0d0d;
+    color: #fff;
+    font-family: 'JetBrainsMono', monospace;
+    padding: 0.75rem 1rem;
+    border-radius: 0.5rem;
+
+    code {
+      color: inherit;
+      padding: 0;
+      background: none;
+      font-size: 0.8rem;
+    }
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+
+  blockquote {
+    padding-left: 1rem;
+    border-left: 2px solid rgba(#0d0d0d, 0.1);
+  }
+
+  hr {
+    border: none;
+    border-top: 2px solid rgba(#0d0d0d, 0.1);
+    margin: 2rem 0;
+  }
 }
 </style>
