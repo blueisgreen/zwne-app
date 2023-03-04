90601867cd2db014d78000be89de7c128eb9c86a
Dave Mount <davemount@gmail.com>
2023-03-04 09:44:16 -0800

    Use svg of Zanzi. Improve landing page.

--- src/pages/PrimaryLandingPage.vue
+++ src/pages/PrimaryLandingPage.vue
@@ -1,19 +1,23 @@
 <template>
-  <q-page padding>
-    <div class="text-h1 q-pt-lg q-pb-md text-primary">
-      Welcome to Zanzibar's World
-    </div>
-    <div class="text-subtitle1 q-pb-lg text-blue-10">
-      Explore the world of nuclear power, the most effective source of energy to
-      help us thrive.
-    </div>
-    <q-img src="icons/favicon-128x128.png" width="25%" />
-    <div class="q-py-sm">
-      I am Zanzibar. Let me show you everything we know about nuclear power.
+  <q-page flex flex-center>
+    <div class="text-center">
+      <div class="text-h1 q-pt-lg q-pb-md text-primary">
+        Welcome to Zanzibar's World
+      </div>
+      <div class="text-subtitle1 q-pb-lg text-blue-10">
+        Explore the world of nuclear power, the most effective source of energy
+        to help us thrive.
+      </div>
+      <q-img :src="Zanzibar" width="50%" />
+      <div class="q-py-sm">
+        I am Zanzibar. Let me show you everything we know about nuclear power.
+      </div>
     </div>
   </q-page>
 </template>
 
-<script setup></script>
+<script setup>
+import Zanzibar from 'assets/Zanzibar.svg'
+</script>
 
 <style lang="scss" scoped></style>
