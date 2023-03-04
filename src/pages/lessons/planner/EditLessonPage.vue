fd7e36f0cb2c85f05d98f6b9f67c989d8eced3de
Dave Mount <davemount@gmail.com>
2023-03-03 17:14:42 -0800

    Clean up experiments. Set up to work on preview.

--- src/pages/lessons/planner/EditLessonPage.vue
+++ src/pages/lessons/planner/EditLessonPage.vue
@@ -12,11 +12,8 @@
 
     <q-tabs v-model="tabModel" class="bg-blue-1 text-primary" dense no-caps>
       <q-tab name="edit" label="Edit" />
-      <q-tab name="build" label="Build" />
-      <q-tab name="demo" label="Demo" />
-      <q-tab name="images" label="Images" />
       <q-tab name="preview" label="Preview" />
-      <q-tab name="html" label="HTML View" />
+      <q-tab name="html" label="HTML" />
       <q-route-tab
         :to="{ name: 'lesson-planner' }"
         label="Back to Planner"
@@ -27,22 +24,13 @@
 
     <q-tab-panels v-model="tabModel">
       <q-tab-panel name="edit">
-        <lesson-composer />
-      </q-tab-panel>
-      <q-tab-panel name="build">
         <lesson-builder />
-      </q-tab-panel>
-      <q-tab-panel name="demo">
-        <lesson-builder-demo />
-      </q-tab-panel>
-      <q-tab-panel name="images">
-        <image-manager />
       </q-tab-panel>
       <q-tab-panel name="preview">
         <lesson-preview />
       </q-tab-panel>
       <q-tab-panel name="html">
-        <div class="text-h6">HTML View</div>
+        <div class="text-h6">HTML</div>
         <q-card>
           <q-card-section>
             <pre style="white-space: pre-line">{{
@@ -60,16 +48,13 @@ import { ref, onBeforeMount, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import { useQuasar } from 'quasar'
 import { useLessonPlannerStore } from 'stores/lesson-planner-store.js'
-import LessonComposer from './LessonComposer.vue'
 import LessonBuilder from './LessonBuilder.vue'
-import LessonBuilderDemo from './LessonBuilderDemo.vue'
-import ImageManager from './ImageManager.vue'
 import LessonPreview from './LessonPreview.vue'
 import LessonEditorSpecs from './LessonEditorSpecs.vue'
 
 const $q = useQuasar()
 const planner = useLessonPlannerStore()
-const tabModel = ref('build')
+const tabModel = ref('edit')
 
 onBeforeMount(() => {
   const lessonId = useRoute().params.id
