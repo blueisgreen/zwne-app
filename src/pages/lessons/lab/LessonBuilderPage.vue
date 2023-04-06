c450f402240a09ab8f8bde40860382d6c7b54688
Dave Mount <davemount@gmail.com>
2023-04-05 22:13:21 -0700

    Toe-step in

--- src/pages/lessons/lab/LessonBuilderPage.vue
+++ src/pages/lessons/lab/LessonBuilderPage.vue
@@ -2,9 +2,9 @@
   <q-page padding>
     <q-toolbar>
       <q-toolbar-title>
-        <div class="text-h4">Lesson {{ lessonId }}</div>
+        <div class="text-h4">Build Lesson</div>
         <div class="text-caption text-secondary">
-          Share lessons for the good humankind.
+          {{ lessonId }}
         </div>
       </q-toolbar-title>
       <q-btn :to="{ name: 'lessonLab' }" color="primary" no-caps label="To Lab Lobby" />
@@ -13,13 +13,17 @@
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import { useLessonLabStore } from 'src/stores/lesson-lab'
 
 const route = useRoute()
 const lab = useLessonLabStore()
-
 const lessonId = route.params.id
+
+onMounted(async () => {
+  await lab.loadLesson(lessonId)
+})
 </script>
 
 <style lang="scss" scoped></style>
