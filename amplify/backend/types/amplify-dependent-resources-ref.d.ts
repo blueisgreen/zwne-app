0c26eb65b5242ac1c1bab5dd4d33901574e1bcd2
Dave Mount <davemount@gmail.com>
2023-04-08 10:33:16 -0700

    Set up auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,5 +5,27 @@ export type AmplifyDependentResourcesAttributes = {
       "GraphQLAPIIdOutput": "string",
       "GraphQLAPIKeyOutput": "string"
     }
+  },
+  "auth": {
+    "userPoolGroups": {
+      "AdminsGroupRole": "string",
+      "EditorsGroupRole": "string",
+      "MembersGroupRole": "string"
+    },
+    "zwapp7e908767": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "zwappContent": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
