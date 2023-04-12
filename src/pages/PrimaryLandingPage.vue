d0e3c45eb9a8fd94975e8737d1d855cd6efa4a88
Dave Mount <davemount@gmail.com>
2023-04-12 09:04:03 -0700

    Try to fix issues with font downloads

--- src/pages/PrimaryLandingPage.vue
+++ src/pages/PrimaryLandingPage.vue
@@ -1,17 +1,15 @@
 <template>
   <q-page flex flex-center>
     <div class="text-center">
-      <div class="text-h1 q-pt-lg q-pb-md text-primary">
-        Welcome to Zanzibar's World
-      </div>
+      <div class="text-h1 mega-title q-pt-lg q-pb-md">Welcome to Zanzibar's World</div>
       <div class="text-subtitle1 q-pb-lg text-blue-10">
-        Explore the world of nuclear power, the most effective source of energy
-        to help us thrive.
+        Explore the world of nuclear power, the most effective source of energy to help us
+        thrive.
       </div>
       <q-img :src="Zanzibar" width="50%" alt="Zanzibar, Nuclear Hero" />
       <div class="q-py-sm">
-        I am Zanzibar. You can call me Zanzi. Let me show you everything we know
-        about nuclear power.
+        I am Zanzibar. You can call me Zanzi. Let me show you everything we know about
+        nuclear power.
       </div>
     </div>
   </q-page>
